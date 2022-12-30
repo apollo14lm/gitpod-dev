69e7bb56f758f3a992fa141df450b88f4a2ccf18
apollo14lm <99069997+apollo14lm@users.noreply.github.com>
2022-12-30 18:54:58 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,24 +9,6 @@ create_env() {
   fi
 }
 
-# appending a env variable to a .env file only if it does not already exist
-append_env() {
-  param=$1
-  #echo "${param}=${!param}"
-  grep -qxF "${param}=${!param}" $UD/dotfile/.env \
-    || echo "${param}=${!param}" >> $UD/dotfile/.env
-}
-
-# add directory to $PATH if it's not already
-append_path() {
-  echo "$1"
-  if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
-    PATH="${PATH:+"$PATH:"}$1"
-    echo "1"
-  fi
-  echo $PATH
-}
-
 if [ -d "$UD" ]; then
   create_env
 
